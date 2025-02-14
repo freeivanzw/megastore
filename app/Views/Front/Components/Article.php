794b09d8f32ba653867ba4454cbebf6544737f41
Ivan Kozak <79371026+freeivanzw@users.noreply.github.com>
2025-02-14 12:05:23 +0200

    Update Article.php

--- app/Views/Front/Components/Article.php
+++ app/Views/Front/Components/Article.php
@@ -2,7 +2,7 @@
     <div class="container py-5">
         <div class="row">
             <article class="col-12">
-                <h1 class="text-center mb-4"><?= $title; ?></h1>
+                <h1 class="text-center mb-4 remote-change"><?= $title; ?></h1>
                 <div class="content">
                     <?= $content; ?>
                 </div>
